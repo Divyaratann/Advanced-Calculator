a355fbe68ad8591d2b51b9152437a5a1fcd55378
Divyaratann srivastava <divyratan2703@gmail.com>
2026-02-23 18:53:23 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,56 @@
+import math
+
+def calculator():
+    print("==== Advanced Calculator ====")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Power")
+    print("6. Square Root")
+    print("7. Percentage")
+    print("0. Exit")
+
+    while True:
+        choice = input("\nEnter your choice: ")
+
+        if choice == "0":
+            print("Exiting Calculator...")
+            break
+
+        elif choice in ["1", "2", "3", "4", "5", "7"]:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == "1":
+                print("Result:", num1 + num2)
+
+            elif choice == "2":
+                print("Result:", num1 - num2)
+
+            elif choice == "3":
+                print("Result:", num1 * num2)
+
+            elif choice == "4":
+                if num2 != 0:
+                    print("Result:", num1 / num2)
+                else:
+                    print("Error! Division by zero.")
+
+            elif choice == "5":
+                print("Result:", num1 ** num2)
+
+            elif choice == "7":
+                print("Result:", (num1 / num2) * 100)
+
+        elif choice == "6":
+            num = float(input("Enter number: "))
+            if num >= 0:
+                print("Result:", math.sqrt(num))
+            else:
+                print("Error! Negative number.")
+
+        else:
+            print("Invalid Choice!")
+
+calculator()
